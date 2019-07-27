4794e030369fe57cb8d6b2936bd2a76491c5d366
grondag <grondag@gmail.com>
2019-07-27 14:56:36 -0700

    Fix refmap - apparent loom bug

--- src/main/java/grondag/clearskies/mixin/MixinBackgroundRenderer.java
+++ src/main/java/grondag/clearskies/mixin/MixinBackgroundRenderer.java
@@ -18,25 +18,39 @@ package grondag.clearskies.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.BackgroundRenderer;
+import net.minecraft.client.render.Camera;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 
 @Mixin(BackgroundRenderer.class)
 public class MixinBackgroundRenderer {
+    
+    //UGLY: only purpose is to force refmap output
+    @SuppressWarnings("unused")
+    @Inject(method = "updateColorNotInWater", at = @At("HEAD"), cancellable = false, require = 1)
+    public void hookUpdateColorNotInWaterHead(Camera camera, World world, float tickDelta, CallbackInfo ci) {
+        final MinecraftClient mc = MinecraftClient.getInstance();
+        if(world.dimension.hasVisibleSky() && !world.isRaining() && 0 == 1) {
+            world.getSkyColor(mc.gameRenderer.getCamera().getBlockPos(), mc.getTickDelta());
+        }
+    }    
+    
     @ModifyVariable(
             at = @At(
-                value = "INVOKE_ASSIGN",
-                target = "Lnet/minecraft/world/World;getFogColor(F)Lnet/minecraft/util/math/Vec3d;"
-            ),
+                    value = "INVOKE_ASSIGN",
+                    target = "Lnet/minecraft/world/World;getFogColor(F)Lnet/minecraft/util/math/Vec3d;"
+                    ),
             method = "updateColorNotInWater",
             ordinal = 1,
             require = 1,
             allow = 1
-        )
+            )
     private Vec3d hookUpdateColorNotInWaterSky(Vec3d val) {
         final MinecraftClient mc = MinecraftClient.getInstance();
         final World world = mc.world;
