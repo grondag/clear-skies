d786e657898fc8d25cfcfdc60c08b12c3bc53186
grondag <>
2021-09-19 10:32:38 -0700

    First pass at Mojang mappings. Untested, likely broken.

--- src/main/java/grondag/clearskies/mixin/MixinBackgroundRenderer.java
+++ src/main/java/grondag/clearskies/mixin/MixinBackgroundRenderer.java
@@ -16,18 +16,17 @@
 
 package grondag.clearskies.mixin;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.client.renderer.FogRenderer;
+import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.render.BackgroundRenderer;
-import net.minecraft.client.world.ClientWorld;
-import net.minecraft.util.math.Vec3d;
-
-@Mixin(BackgroundRenderer.class)
+@Mixin(FogRenderer.class)
 public class MixinBackgroundRenderer {
 	@Shadow private static float red;
 	@Shadow private static float green;
@@ -37,12 +36,12 @@ public class MixinBackgroundRenderer {
 	@Shadow private static long lastWaterFogColorUpdateTime = -1L;
 
 	@ModifyVariable(at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/util/CubicSampler;sampleColor(Lnet/minecraft/util/math/Vec3d;Lnet/minecraft/util/CubicSampler$RgbFetcher;)Lnet/minecraft/util/math/Vec3d;"), method = "render", ordinal = 2, require = 1, allow = 1)
-	private static Vec3d onSampleColor(Vec3d val) {
-		final MinecraftClient mc = MinecraftClient.getInstance();
-		final ClientWorld world = mc.world;
+	private static Vec3 onSampleColor(Vec3 val) {
+		final Minecraft mc = Minecraft.getInstance();
+		final ClientLevel world = mc.level;
 
-		if (world.getDimension().hasSkyLight()) {
-			return world.method_23777(mc.gameRenderer.getCamera().getPos(), mc.getTickDelta());
+		if (world.dimensionType().hasSkyLight()) {
+			return world.getSkyColor(mc.gameRenderer.getMainCamera().getPosition(), mc.getFrameTime());
 		} else {
 			return val;
 		}
@@ -54,12 +53,12 @@ public class MixinBackgroundRenderer {
 	}
 
 	@Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/world/ClientWorld;getRainGradient(F)F"), method = "render", require = 1, allow = 1)
-	private static float onGetRainGradient(ClientWorld world, float tickDelta) {
+	private static float onGetRainGradient(ClientLevel world, float tickDelta) {
 		return 0;
 	}
 
 	@Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/world/ClientWorld;getThunderGradient(F)F"), method = "render", require = 1, allow = 1)
-	private static float onGetThunderGradient(ClientWorld world, float tickDelta) {
+	private static float onGetThunderGradient(ClientLevel world, float tickDelta) {
 		return 0;
 	}
 }
