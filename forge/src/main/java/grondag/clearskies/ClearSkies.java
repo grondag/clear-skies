72b87fbe91badca966ea41e8ee370e58609ea963
grondag <grondag@users.noreply.github.com>
2021-12-13 18:26:06 -0800

    Update license, gruntle stuff

--- forge/src/main/java/grondag/clearskies/ClearSkies.java
+++ forge/src/main/java/grondag/clearskies/ClearSkies.java
@@ -1,5 +1,9 @@
 /*
- * Copyright © Original Authors
+ *
+ * This file is part of Clear Skies and is licensed to the project under
+ * terms that are compatible with the GNU Lesser General Public License.
+ * See the NOTICE file distributed with this work for additional information
+ * regarding copyright ownership and licensing.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -13,9 +17,6 @@
  *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * Additional copyright and licensing notices may apply for content that was
- * included from other projects. For more information, see ATTRIBUTION.md.
  */
 
 package grondag.clearskies;
